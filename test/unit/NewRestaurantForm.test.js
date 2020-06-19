c5f433204d14db1c6eef017ff0271bc59ac65c92
IuriVeloso <iuriv.felix@gmail.com>
2020-06-19 10:21:02 -0300

    CI

--- test/unit/NewRestaurantForm.test.js
+++ test/unit/NewRestaurantForm.test.js
@@ -4,18 +4,18 @@ import NewRestaurantForm from '../../src/NewRestaurantForm.js';
 import "../../node_modules/materialize-css/dist/js/materialize.min.js";
 
 describe("NewRestaurantForm", () => {
-    describe('clicking the save button', () => {
-        it('calls the onSave handler', () => {
-            const saveHandler = jest.fn();
-            const wrapper = mount(<NewRestaurantForm onSave={saveHandler}/>);
+  describe('clicking the save button', () => {
+    it('calls the onSave handler', () => {
+      const saveHandler = jest.fn();
+      const wrapper = mount(<NewRestaurantForm onSave={saveHandler}/>);
 
-            wrapper.find('input[data-test="newRestaurantName"]')
-                .simulate('change', { target: { value: 'Sushi Place' } });
+      wrapper.find('input[data-test="newRestaurantName"]')
+        .simulate('change', { target: { value: 'Sushi Place' } });
 
-            wrapper.find('button[data-test="saveNewRestaurantButton"]')
-                .simulate('click');
+      wrapper.find('button[data-test="saveNewRestaurantButton"]')
+        .simulate('click');
 
-            expect(saveHandler).toHaveBeenCalledWith('Sushi Place');
-        });
+      expect(saveHandler).toHaveBeenCalledWith('Sushi Place');
     });
+  });
 });
